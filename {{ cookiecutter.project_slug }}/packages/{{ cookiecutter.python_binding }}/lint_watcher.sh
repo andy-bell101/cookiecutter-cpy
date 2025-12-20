ae889ca865fa01b69e70d7468faff3a666c504e1
Andrew Bell <andrew.bell012276@gmail.com>
2025-12-20 18:21:40 +0000

    fixup! testing template

--- {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/lint_watcher.sh
+++ {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/lint_watcher.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cmake --preset gcc-debug
 cmake --build --preset build-gcc-debug --target tests
 clang-tidy -p build/gcc-debug $(git ls-files '*.cpp' '*.h')
--- {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/test_watcher.sh
+++ {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/test_watcher.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cmake --preset gcc-debug
 cmake --build --preset build-gcc-debug --target tests
 ctest --test-dir build/gcc-debug --output-on-failure
