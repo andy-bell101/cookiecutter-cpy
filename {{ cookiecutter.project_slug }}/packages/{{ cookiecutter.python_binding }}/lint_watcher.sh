5c9b960535e564ebe8ea6fc2f8ff99445df10b6b
Andrew Bell <andrew.bell012276@gmail.com>
2025-12-20 19:31:24 +0000

    remove watchers and custom zellij layout

--- {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/lint_watcher.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cmake --preset gcc-debug
-cmake --build --preset build-gcc-debug --target tests
-clang-tidy -p build/gcc-debug $(git ls-files '*.cpp' '*.h')
--- {{ cookiecutter.project_slug }}/packages/{{ cookiecutter.python_binding }}/test_watcher.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cmake --preset gcc-debug
-cmake --build --preset build-gcc-debug --target tests
-ctest --test-dir build/gcc-debug --output-on-failure
